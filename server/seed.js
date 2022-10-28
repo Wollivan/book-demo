537c4ecf2775185fd829a7bae623336550eb6297
Wollivan <tim.smith2814@gmail.com>
2022-10-28 21:43:29 +0100

    Correct api url

--- server/seed.js
+++ server/seed.js
@@ -1,20 +1,48 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-const Book = require("./models/book");
+const Book = require("./src/models/book");
 
 mongoose.connect(process.env.DATABASE_URL);
+const bookStarts = [
+  [
+    {
+      title: "The Silmarilion",
+      description: "Basically the Bible except its fantasy.",
+      year: "1977",
+      isbn: "9780618391110",
+    },
+    {
+      title: "Twilight",
+      description: "Shiny vampires",
+      year: "2009",
+      isbn: "9780316015844",
+    },
+    {
+      title: "The  Hobbit",
+      author: "JRR Tolkien",
+      description: "Bilbo does a nusiance.",
+      year: "1937",
+      isbn: "9780547928227",
+    },
+    {
+      title: "The  Fellowship of the Ring",
+      author: "JRR Tolkien",
+      description: "Frodo does a nusiance.",
+      year: "1954",
+      isbn: "0261102354",
+    },
+  ],
+];
 
 async function seed() {
-  await Book.create({
-    title: "The Hobbit",
-    description: "A small man goes and kills a dragon or something...",
-    year: "1937",
-  });
-
-  await Book.create({
-    title: "The Silmarilion",
-    description: "Basically the Bible except its fantasy.",
-    year: "1977",
+  bookStarts.forEach(async (book) => {
+    await Book.create({
+      title: book.title,
+      author: book.author,
+      description: book.description,
+      year: book.year,
+      isbn: book.isbn,
+    });
   });
 
   console.log("Books summoned!");
